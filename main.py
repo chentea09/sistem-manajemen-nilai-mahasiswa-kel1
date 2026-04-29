d53145c5f48042ea3d81172749c45aa360d379c8
gadis sarah al ziqri <gadissarahalziqri02@gmail.com>
2026-04-29 11:47:38 +0700

    komentar lagi

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ m1 = Mahasiswa("Chen", "23001")
 m2 = Mahasiswa("Budi", "23002")
 m3 = Mahasiswa("Siti", "23003")
 
+# mengisi nilai tugas, uts, dan uas mahasiswa
 m1.set_nilai(80, 85, 90)
 m2.set_nilai(70, 75, 80)
 m3.set_nilai(60, 65, 70)
 
+# menghitung nilai akhir mahasiswa 
 m1.hitung_nilai_akhir()
 m2.hitung_nilai_akhir()
 m3.hitung_nilai_akhir()
 
+# menampilkan hasil mahasiswa
 print("DATA MAHASISWA")
 m1.info()
 m2.info()
